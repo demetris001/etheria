76b5a9b108be508136e458fa1a6dc2564fa09fd9
Dimitrios Christodoulou <demetris001@gmail.com>
2026-06-11 02:23:08 +0300

    Configure email settings from environment

--- etheria/settings.py
+++ etheria/settings.py
@@ -150,6 +150,12 @@ AUTHENTICATION_BACKENDS = [
 ]
 ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_URL='/enter/'
 LOGIN_REDIRECT_URL = '/my-trips/'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
